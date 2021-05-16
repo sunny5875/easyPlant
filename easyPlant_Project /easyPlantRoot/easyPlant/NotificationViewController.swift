941d8236915780c4849a207bca914ae952440a04
dycha0430 <dycha0430@hanyang.ac.kr>
2021-05-16 17:52:43 +0900

    home tab edit

--- easyPlant_Project /easyPlantRoot/easyPlant/NotificationViewController.swift
+++ easyPlant_Project /easyPlantRoot/easyPlant/NotificationViewController.swift
@@ -9,33 +9,32 @@ import UIKit
 
 class NotificationViewController: UIViewController {
     
-    @IBOutlet weak var colorLabel: UILabel!
-    var myPlant: userPlant?
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var cycleLabel: UILabel!
+    
+    @IBOutlet weak var colorPickerView: UIColorWell!
+    
+    var myPlant: userPlant?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "알림"
-        colorLabel.text = "빨강"
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
         if let myPlant = myPlant {
-            titleLabel.text = "\(myPlant.name) 물주기"
-            cycleLabel.text = String(myPlant.waterPeriod)
+            titleLabel.text = "\(myPlant.name) 물 주기"
+        } else {
+            titleLabel.text = "잘못된 정보입니다."
         }
+        colorPickerView.addTarget(self, action: #selector(colorWellChanged(_:)), for: .valueChanged)
+        
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func colorWellChanged(_ sender: Any) {
+        myPlant?.color = colorPickerView.selectedColor ?? UIColor(cgColor: CGColor(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        
+        
     }
-    */
-
+    
+    
 }
