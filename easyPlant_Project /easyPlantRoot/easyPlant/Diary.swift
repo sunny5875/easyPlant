74dace9d89fe21f35fd3c8eb3c92427748fd0992
sunny5875 <55349553+sunny5875@users.noreply.github.com>
2021-06-04 14:54:35 +0900

    버그 수정

--- easyPlant_Project /easyPlantRoot/easyPlant/Diary.swift
+++ easyPlant_Project /easyPlantRoot/easyPlant/Diary.swift
@@ -21,6 +21,6 @@ var diarys : [Diary] = [Diary(title: "hello 초록콩",date: "2020-10-31", story
                         Diary(title: "hello 초록콩2",date: "2020-12-30", story: "오늘은 초로콩이 온지 두달 지난날>ㅁ<", picture: "유칼립투스"),Diary(title: "hello 초록콩",date: "2020-10-31", story: "오늘은 초로콩이 온지 하루 지난날>ㅁ<", picture: "바이올렛"),
                         Diary(title: "hello 초록콩1", date: "2020-11-30", story: "오늘은 초로콩이 온지 한달 지난날>ㅁ<", picture: "문샤인"),
                         Diary(title: "hello 초록콩2",date: "2020-12-30", story: "오늘은 초로콩이 온지 두달 지난날>ㅁ<", picture: "극락조"),
-                        Diary(title: "hello 초록콩",date: "2020-10-31", story: "오늘은 초로콩이 온지 하루 지난날>ㅁ<", picture: "트리안"),
-                                                Diary(title: "hello 초록콩1", date: "2020-11-30", story: "오늘은 초로콩이 온지 한달 지난날>ㅁ<", picture: "페페"),
-                                                Diary(title: "hello 초록콩2",date: "2020-12-30", story: "오늘은 초로콩이 온지 두달 지난날>ㅁ<", picture: "율마")]
+                        Diary(title: "hello 초록콩",date: "2020-10-31", story: "오늘은 초로콩이 온지 하루 지난날>ㅁ<", picture: "plant"),
+                                                Diary(title: "hello 초록콩1", date: "2020-11-30", story: "오늘은 초로콩이 온지 한달 지난날>ㅁ<", picture: "plant"),
+                                                Diary(title: "hello 초록콩2",date: "2020-12-30", story: "오늘은 초로콩이 온지 두달 지난날>ㅁ<", picture: "plant")]
