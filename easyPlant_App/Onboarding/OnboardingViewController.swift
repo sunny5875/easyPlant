5ed1b6f7b9d25a414c9f4d032cfdf22f0786755c
sunny5875 <55349553+sunny5875@users.noreply.github.com>
2021-07-12 15:30:19 +0900

    onboarding 추가

---
+++ easyPlant_App/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  easyPlant
+//
+//  Created by 현수빈 on 2021/07/12.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
