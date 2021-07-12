ca13ff79d3cde9d0a082bafb59daba74635a94ca
dycha0430 <dycha0430@hanyang.ac.kr>
2021-07-12 23:35:52 +0900

    레벨뷰 UI 수정

--- easyPlant_App/easyPlant/Home/LevelViewController.swift
+++ easyPlant_App/easyPlant/Home/LevelViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LevelViewController: UIViewController {
 
+    @IBOutlet weak var bgView: UIView!
     @IBOutlet weak var levelStack: UIStackView!
     @IBOutlet weak var myStack: UIStackView!
     @IBOutlet var levelLables: [UILabel]!
@@ -22,32 +23,46 @@ class LevelViewController: UIViewController {
     
         super.viewDidLoad()
 
-        let attributedString = NSMutableAttributedString(string: "당신은 현재 \(myUser.level.name). \n\(myUser.level.description)")
+        myStack.layer.zPosition = 102
+        levelStack.layer.zPosition = 101
+        bgView.layer.zPosition = 100
+        
+        
+        
+        let attributedString = NSMutableAttributedString(string: "당신은 현재 \(myUser.level.name)! \n\(myUser.level.description)")
 
         // *** Create instance of `NSMutableParagraphStyle`
         let paragraphStyle = NSMutableParagraphStyle()
 
         // *** set LineSpacing property in points ***
-        paragraphStyle.lineSpacing = 6 // Whatever line spacing you want in points
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineSpacing = 7 // Whatever line spacing you want in points
+        
         
         // *** Apply attribute to string ***
         attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range:NSMakeRange(0, attributedString.length))
 
         attributedString.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 15), range: NSMakeRange(0, attributedString.length))
 
+        myLevelView.textColor = UIColor.white
         myLevelView.attributedText = attributedString
         myLevelView.layer.cornerRadius = 30
-        print(myLevelView.text!)
+        
         myStack.layer.cornerRadius = 30
-        myStack.backgroundColor = UIColor(cgColor: CGColor(red: 174/255, green: 213/255, blue: 129/255, alpha: 1))
+        //myStack.backgroundColor = UIColor(cgColor: CGColor(red: 174/255, green: 213/255, blue: 129/255, alpha: 1))
 
-        myLevelView.backgroundColor = UIColor(cgColor: CGColor(red: 174/255, green: 213/255, blue: 129/255, alpha: 1))
+        //myLevelView.backgroundColor = UIColor(cgColor: CGColor(red: 174/255, green: 213/255, blue: 129/255, alpha: 1))
+        
+        myStack.backgroundColor = UIColor.white
+        myLevelView.backgroundColor = UIColor.clear
+        
+        myStack.layer.shadowOpacity = 0.2
+        myStack.layer.shadowOffset = CGSize(width: 0, height: 10)
+        myStack.layer.shadowRadius = 30
+        myStack.layer.masksToBounds = false
 
-
-        levelStack.layer.borderWidth = 1
-        levelStack.layer.borderColor = UIColor.white.cgColor
         levelStack.layer.cornerRadius = 15
-        levelStack.backgroundColor = UIColor.white
+        levelStack.backgroundColor = UIColor.clear
     
         for (i, level) in levelLables.enumerated() {
             print(i)
@@ -62,7 +77,7 @@ class LevelViewController: UIViewController {
                 level.text = "평균  행복도: \(levels[i].hapiness) 키운 식물수: \(levels[i].numPlants) 키운 기간: \(levels[i].growingDays)"
             }
             //level.font = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
-            level.backgroundColor =  UIColor.white
+            level.backgroundColor =  UIColor.clear
         }
         
         for (i, level) in levelImages.enumerated() {
