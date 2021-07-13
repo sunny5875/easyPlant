4cd557aaa861bb0a4ff289c3cb72697c99bead91
iQuQi <69742775+iQuQi@users.noreply.github.com>
2021-07-13 11:32:32 +0900

    change ui

--- easyPlant_App/easyPlant/Login/LoginViewController.swift
+++ easyPlant_App/easyPlant/Login/LoginViewController.swift
@@ -36,12 +36,12 @@ class LoginViewController: UIViewController {
             pwField.placeholder = "이미 로그인 된 상태입니다."
         }
         
-        findBtn.layer.borderColor = UIColor.systemGray4.cgColor
-        findBtn.layer.borderWidth = 1
+        //findBtn.layer.borderColor = UIColor.systemGray4.cgColor
+        //findBtn.layer.borderWidth = 1
         findBtn.layer.cornerRadius = 15
         
-        joinBtn.layer.borderColor = UIColor.systemGray4.cgColor
-        joinBtn.layer.borderWidth = 1
+        //joinBtn.layer.borderColor = UIColor.systemGray4.cgColor
+        //joinBtn.layer.borderWidth = 1
         joinBtn.layer.cornerRadius = 15
 
     }
